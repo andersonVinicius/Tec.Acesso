c5a39556b00cf3bfe78e4a6a0a3784c7d36ac6d4
andersonVinicius <anderson.freitas09@gmail.com>
2017-05-22 20:34:22 -0300

    Update calcula_cabo2.m

--- calcula_cabo2.m
+++ calcula_cabo2.m
@@ -3,11 +3,10 @@
 %Codigo que calcula taxa de tranferencia de um cabo DSL com varia sec�oes
 %Bitola:[0.4 0.5 0.63 0.90]
 %diferentes ou n�o
-=======
 %Codigo que calcula taxa de tranferencia de um cabo DSL com varia secçoes
 %Bitola:[0.4 0.5 0.63 0.90]
 %diferentes ou não
->>>>>>> 5fcb076f84c1347179e08fb3001aa20ba8a607a8
+
 %Colaboradores: Edemir e Thiago v1
 %               Rafael          v2
 %               Vitoria         v3
@@ -24,20 +23,20 @@ epson0 = 8.85418782*10^-12; % Permissividade eletrica do vacuo A^2 s^2 kg^-1 m^-
 =======
 m0= 1.2566*(10^(-6)); % permeabilidade magnetica do vacuo H/m (ou TÂ·m/A).
 epson0 = 8.85418782*10^-12; % Permissividade eletrica do vacuo.
->>>>>>> 5fcb076f84c1347179e08fb3001aa20ba8a607a8
+
 %-------------------------------------------------------------------------
 
 
 % Valores por tipo-cabo
-<<<<<<< HEAD
+
 s = [0.3 0.19 0.31 0.41]/1000; %Espessura do Isolante m
 d = [0.4 0.51 0.64 0.96]/1000; %Diametro do condutor m
 v = [100 100 100 100]; %
-=======
+
 s = [0.3 0.19 0.31 0.41]/1000 %Espessura do Isolante
 d = [0.4 0.51 0.64 0.96]/1000; %Diametro do condutor
 v = [100 100 100 100];
->>>>>>> 5fcb076f84c1347179e08fb3001aa20ba8a607a8
+
 epsonM = [2.115 2.115 2.197 2.115]; 
 epsonS = [2.3 2.3 2.2 2.3]; 
 t = [2*10^(-20) 2*10^(-20) 6*10^(-4) 2*10^(-20)];
@@ -45,7 +44,7 @@ a = [0.87 0.87 0.53 0.87];
 b = [0.25 0.25 0.14 0.25];
 
 
-<<<<<<< HEAD
+
 %calculo do da sec��o
 r = 1 + (2 * s(u) / d(u));
 r0 = 4 / (pi * sigma * ( d(u) ^ 2 )); 
@@ -65,7 +64,7 @@ zs = x1 + z1; %Imped�ncia s�rie de um par-tran�ado sem o efeito do tran�
 
 z0 = sqrt(zs./yp); %Imped�ncia caracteristica em funcao da frequencia
 gama = sqrt(zs.* yp.*(1+(pi*v(u)*r*d(u))^2)); %
-=======
+
 %calculo do da secção
 r = 1 + (2 * s(u) / d(u));
 r0 = 4 / (pi * sigma * ( d(u) ^ 2 )); 
@@ -84,7 +83,6 @@ zs = x1 + z1; %Impedância série de um par-trançado sem o efeito do trançado
 
 z0 = sqrt(zs./yp); %Impedância caracteristica em funcao da frequencia
 gama = sqrt(zs .* yp .* (1+(pi*v(u)*r*d(u))^2) ); %
->>>>>>> 5fcb076f84c1347179e08fb3001aa20ba8a607a8
 
 
 end
